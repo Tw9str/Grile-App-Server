aac15521ba6f284f21b05d5f5f1c8a4357d60653
tw9str <i88b@hotmail.com>
2024-08-12 17:21:24 +0200

    fixed payment api

--- routes/stripeRoutes.js
+++ routes/stripeRoutes.js
@@ -9,7 +9,7 @@ const verifyRole = require("../middleware/auth");
 const router = express.Router();
 
 router.post("/checkout", checkout);
-router.post("/webhook", express.raw({ type: "application/json" }), webhook);
+router.post("/webhook", webhook);
 router.post(
   "/customer-portal",
   verifyRole(["student", "teacher", "admin"]),
